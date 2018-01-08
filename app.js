5d3e4e485b251b9a48c649263ba0e67d5e7ffea6
nathan-qwizdom <nathan.mcclelland@qwizdom.co.uk>
2018-01-08 12:52:10 +0000

    Full monster slayer game.

---
+++ app.js
@@ -0,0 +1,102 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+    turns: []
+  },
+  methods: {
+    startGame :function(){
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.turns = [];
+    },
+    attack: function(){
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      // Shifts all elements in the array to the right.
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits for ' + damage
+      });
+      if(this.checkWin())
+      {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    specialAttack: function(){
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player special hits for ' + damage
+      });
+      if(this.checkWin())
+      {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    heal: function(){
+      if(this.playerHealth <= 90)
+      {
+        this.playerHealth += 10;
+      }
+      else
+      {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heal for 10'
+      });
+      this.monsterAttacks();
+    },
+    giveUp: function(){
+      this.gameIsRunning = false;
+    },
+    calculateDamage: function(min, max){
+      // The Math.floor() function returns the largest integer less than or equal to a given number.
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function(){
+      if(this.monsterHealth <= 0)
+      {
+        if(confirm('You won! New game?'))
+        {
+          this.startGame();
+        }
+        else
+        {
+            this.gameIsRunning = false;
+        }
+        return true;
+      }
+      else if (this.playerHealth <= 0)
+      {
+        if(confirm('You lost! New game?'))
+        {
+          this.startGame();
+        }
+        else
+        {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    monsterAttacks: function(){
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits for ' + damage
+      });
+    }
+  }
+});
